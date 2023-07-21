1027491db06d10dd11ef219160ce93fef3ecd0c2
nguyễnmihn <Nminh59001@gmail.com>
2023-07-21 11:20:10 +0700

    project

---
+++ MDK-ARM/RTE/_bai11_uart_it_dma/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'bai11_uart_it_dma' 
+ * Target:  'bai11_uart_it_dma' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
